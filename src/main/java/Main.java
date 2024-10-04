98deeab9a964850caba35c50ff7b4cd4c14af557
Taty ts <tatytsareva@mail.ru>
2024-10-04 11:51:40 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import ru.netology.javaqa.mvn2qa96.services.HolidayService;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+
+
+
+
---
+++ src/test/java/ru/netology/javaqa/mvn2qa96/services/HolidayServiceTest.java
@@ -0,0 +1,50 @@
+package ru.netology.javaqa.mvn2qa96.services;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+public class HolidayServiceTest {
+
+    @ParameterizedTest
+//    @CsvSource({
+//            "3,10000,3000,20000",
+//            "2,100000,60000,150000"
+//    })
+    @CsvFileSource(files = "src/test/resources/mouth.csv")
+
+
+    public void RestMouthTest(int expected, int expenses, int threshold, int income) {
+
+        HolidayService service = new HolidayService();
+
+        int actual = service.calculate(expenses,threshold,income);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+//    @Test
+//    public void RestMouthTest2() {
+//
+//        HolidayService service = new HolidayService();
+//        int expected = 3;
+//
+//        int actual = service.calculate(10_000, 3_000, 20_000);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+
+}
+
+
+
+
+
+
+
+
+
